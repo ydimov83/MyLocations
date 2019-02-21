c40d21f3907aba6d18978412f34dc91f6db149aa
Yavor Dimov <ydimov@gmail.com>
2019-02-21 09:22:35 -0700

    Chapter 25 complete, LocationDetailsViewController & CategoryPickerViewController

---
+++ MyLocations/CategoryPickerViewController.swift
@@ -0,0 +1,88 @@
+//
+//  CategoryPickerViewController.swift
+//  MyLocations
+//
+//  Created by Yavor Dimov on 2/21/19.
+//  Copyright © 2019 Yavor Dimov. All rights reserved.
+//
+
+import UIKit
+
+class CategoryPickerViewController: UITableViewController {
+    
+    var selectedCategoryName = ""
+    var selectedIndexPath = IndexPath()
+    
+    let categories = [
+    "No Category",
+    "Apple Store",
+    "Bar",
+    "Bookstore",
+    "Club",
+    "Grocery Store",
+    "Historic Building",
+    "House",
+    "Icecream Vendor",
+    "Landmark",
+    "Park" ]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 0..<categories.count {
+            if categories[i] == selectedCategoryName {
+                selectedIndexPath = IndexPath(row: i, section: 0)
+                break //Once we know which row the currently selected category belongs to, quit
+            }
+        }
+    }
+
+ //    MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return categories.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let categoryName = categories[indexPath.row]
+        
+        cell.textLabel?.text = categoryName
+        
+        if categoryName == selectedCategoryName {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row != selectedIndexPath.row {
+            if let newCell = tableView.cellForRow(at: indexPath) {
+                newCell.accessoryType = .checkmark
+            }
+            if let oldCell = tableView.cellForRow(at: selectedIndexPath) {
+                oldCell.accessoryType = .none
+            }
+            selectedIndexPath = indexPath
+        }
+    }
+    
+    // MARK - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //Used as part of the unwing segue to send the picked cateogry back to LocationDetailsViewController
+        
+        if segue.identifier == "PickedCategory" {
+            let cell = sender as! UITableViewCell
+            if let indexPath = tableView.indexPath(for: cell) {
+                selectedCategoryName = categories[indexPath.row]
+            }
+        }
+    }
+
+}
